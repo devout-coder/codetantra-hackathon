8587bae4c83a9f719b54dbe642fa637e1db11ccd
SPITVedant <101183928+SPITVedant@users.noreply.github.com>
2022-07-26 18:14:07 +0530

    before mergin with pranil's main; navbar added; links to be added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
 import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Navbar whichActive={"none"} />
+      {/* <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
         </Routes>
-      </Router>
+      </Router> */}
     </div>
   );
 }
