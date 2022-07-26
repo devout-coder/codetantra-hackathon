76579d2c90e9748e828ecf628a23cd9b50327361
devout-coder <awessssomepro@gmail.com>
2022-07-26 22:18:21 +0530

    finished the logic of list adoptions page and chat window

--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -2,8 +2,79 @@ import React from "react";
 import "./ChatWindow.css";
 import { Close } from "@material-ui/icons";
 import { IconButton } from "@material-ui/core";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import { getAuth } from "firebase/auth";
 
 const ChatWindow = ({ closeChatWindow }) => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  const [value, setValue] = React.useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    sendMessage(user, value);
+    setValue("");
+  };
+
+  async function sendMessage(user, text) {
+    try {
+      await addDoc(collection(db, "adoption_messages"), {
+        uid: user.uid,
+        displayName: user.displayName,
+        text: text.trim(),
+        timestamp: serverTimestamp(),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function getMessages(callback) {
+    return onSnapshot(
+      query(collection(db, "adoption_messages"), orderBy("timestamp", "asc")),
+      (querySnapshot) => {
+        const messages = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        callback(messages);
+      }
+    );
+  }
+
+  function useMessages() {
+    const [messages, setMessages] = React.useState([]);
+
+    React.useEffect(() => {
+      const unsubscribe = getMessages(setMessages);
+      return unsubscribe;
+    }, []);
+
+    return messages;
+  }
+
+  const containerRef = React.useRef(null);
+  const messages = useMessages();
+
+  React.useLayoutEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  });
+
   function close() {
     //this new class removeModal will be added which has a cool slideback animation attached to it
     document.getElementsByClassName("chat")[0].classList.add("removeChat");
@@ -11,6 +82,7 @@ const ChatWindow = ({ closeChatWindow }) => {
       closeChatWindow();
     }, 800);
   }
+
   return (
     <div className="chatBackground">
       <div className="chat">
@@ -18,9 +90,46 @@ const ChatWindow = ({ closeChatWindow }) => {
         <IconButton onClick={close}>
           <Close />
         </IconButton>
+        <div className="message-list-container" ref={containerRef}>
+          <ul className="message-list">
+            {messages.map((x) => (
+              <Message
+                key={x.id}
+                message={x}
+                isOwnMessage={x.uid === user.uid}
+              />
+            ))}
+          </ul>
+        </div>
+        <div className="messageCont">
+          <form onSubmit={handleSubmit} className="message-input-container">
+            <input
+              type="text"
+              placeholder="Enter a message"
+              value={value}
+              onChange={handleChange}
+              className="message-input"
+              required
+              minLength={1}
+            />
+            <button type="submit" disabled={value < 1} className="send-message">
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
 };
+
+function Message({ message, isOwnMessage }) {
+  const { displayName, text } = message;
+  return (
+    <li className={["message", isOwnMessage && "own-message"].join(" ")}>
+      <h4 className="sender">{isOwnMessage ? "You" : displayName}</h4>
+      <div>{text}</div>
+    </li>
+  );
+}
 
 export default ChatWindow;
