ebcaaaefcd91c3f49821ed5008e6dc7b4572730e
devout-coder <awessssomepro@gmail.com>
2022-07-26 15:26:22 +0530

    incompletely built navbar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 import "./Home.css";
 
 function Home() {
-  return <div className="landing">Fuck you</div>;
+  return (
+    <div className="home-container">
+      <Navbar />
+    </div>
+  );
 }
 
 export default Home;
